eedae31b6e47679749682e3aa2134e180afe70c5
Stephen <stephen@behnfeldt.pro>
2024-09-05 09:05:34 -0400

    Added Blade template for "View Account"

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,1 +1,12 @@
-Account
+<x-app-layout>
+    <x-slot name="header">
+        <h2>{{ __('Account') }}</h2>
+
+        <a class="button" href="{{route('accounts.index')}}">&larr;{{ __('Back') }}</a>
+    </x-slot>
+
+    <h3><strong>{{ $account->company . ' ' . $account->identifier  }}</strong></h3>
+
+    <p>{{$account->description}}</p>
+
+</x-app-layout>
