4dafa9f0266377279b43107a85d9ba07efe7aa6a
Stephen <stephen@behnfeldt.pro>
2024-09-05 07:28:50 -0400

    Removed tailwind from "app" layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,27 +14,24 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js', 'public/style.css'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+    <body>
+        @include('layouts.navigation')
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+        <!-- Page Heading -->
+        @isset($header)
+            <header>
+                {{ $header }}
+            </header>
+        @endisset
 
-            @isset($scripts)
-                {{ $scripts }}
-            @endisset
-        </div>
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
+
+        @isset($scripts)
+            {{ $scripts }}
+        @endisset
     </body>
 </html>
