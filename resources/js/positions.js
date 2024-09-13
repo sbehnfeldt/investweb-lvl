407510f233a0ed77c6e087f64a92a7f1c90dd0d3
Stephen <stephen@behnfeldt.pro>
2024-09-13 09:28:15 -0400

    Display current value (with quantity and price) for all funds on Positions page

--- resources/js/positions.js
+++ resources/js/positions.js
@@ -2,10 +2,11 @@ import $ from 'jquery';
 import TransactionsApi from "@/transactions-api.js";
 import FundsApi from "@/funds-api.js";
 import AccountsApi from "@/accounts-api.js";
+import QuotesApi from "@/quotes-api.js";
 
 $(async function () {
 
-    const P = [AccountsApi.accounts(), FundsApi.funds(), TransactionsApi.transactions()]
+    const P = [AccountsApi.accounts(), FundsApi.funds(), TransactionsApi.transactions(), QuotesApi.latest()]
     Promise.all(P)
         .then((values) => {
             // Build an array mapping fund ID to fund
@@ -13,15 +14,22 @@ $(async function () {
             values[1].forEach((fund) => {
                 funds[fund.id] = fund;
             });
-            console.log(funds);
+
+            // Build an array mapping fund ID to quote
+            const quotes = [];
+            values[3].forEach((quote) => {
+                quotes[quote.fund_id] = quote;
+            });
 
             // Iterate through the Accounts
             const $accounts = $('.accounts');
             values[0].forEach((account) => {
                 const $account = $('<div class="account">');
+                const $h2      = $('<h2>').text(`${account.company}  ${account.description} (${account.identifier})`);
 
-                $account.append('<h2>').text(`${account.company}  ${account.description} ( ${account.identifier})`);
-                $accounts.append($account);
+
+                // $account.append($('<h2>');
+                $account.append($h2);
 
                 // This account's transactions
                 const transactions = values[2].filter((el) => {
@@ -29,18 +37,21 @@ $(async function () {
                 });
 
 
+                const $ul = $('<ul>');
                 values[1].forEach((fund) => {
                     const t = transactions.filter((el) => {
                         return el.fund_id == fund.id;
                     });
-                    console.log(t);
                     if (!t.length) {
                         return;
                     }
 
                     const q = t.reduce((acc, val) => acc + val.quantity, 0);
-                    $account.append($('<p>').text(`${fund.symbol}: ${q}`))
+
+                    $ul.append($('<li>').text(`${fund.symbol}: $${(q * quotes[fund.id].price).toFixed(2)} (${q.toFixed(3)} @ $${(quotes[fund.id].price).toFixed(2)})`))
                 });
+                $account.append($ul);
+                $accounts.append($account);
             });
         })
         .catch((err) => {
