94ef7361e08e373b4d3a0b007d96bb71419d092f
Stephen <stephen@behnfeldt.pro>
2024-09-13 09:38:34 -0400

    Display positions in a table, rather than as a list, on Positions page

--- resources/js/positions.js
+++ resources/js/positions.js
@@ -6,6 +6,41 @@ import QuotesApi from "@/quotes-api.js";
 
 $(async function () {
 
+    const buildTable = (funds, quotes, transactions) => {
+        let $table = $('<table>');
+
+        let $thead = $('<thead>');
+        let $tr    = $('<tr>');
+        $tr.append($('<th>').text('Fund'));
+        $tr.append($('<th>').text('Current Value'));
+        $tr.append($('<th>').text('Quantity'));
+        $tr.append($('<th>').text('Current Price'));
+        $thead.append($tr);
+        $table.append($thead);
+
+        let $tbody = $('<tbody>');
+
+        funds.forEach((fund) => {
+            const t = transactions.filter((el) => {
+                return el.fund_id == fund.id;
+            });
+            if (!t.length) {
+                return;
+            }
+
+            const q = t.reduce((acc, val) => acc + val.quantity, 0);
+
+            const $tr = $('<tr>');
+            $tr.append($('<td>').text(`${fund.symbol}`));
+            $tr.append($('<td>').text(`$${(q * quotes[fund.id].price).toFixed(2)}`));
+            $tr.append($('<td>').text(`${q.toFixed(3)}`));
+            $tr.append($('<td>').text(`$${(quotes[fund.id].price).toFixed(2)}`));
+            $tbody.append($tr);
+        });
+        $table.append($tbody);
+        return $table;
+    }
+
     const P = [AccountsApi.accounts(), FundsApi.funds(), TransactionsApi.transactions(), QuotesApi.latest()]
     Promise.all(P)
         .then((values) => {
@@ -25,10 +60,8 @@ $(async function () {
             const $accounts = $('.accounts');
             values[0].forEach((account) => {
                 const $account = $('<div class="account">');
+
                 const $h2      = $('<h2>').text(`${account.company}  ${account.description} (${account.identifier})`);
-
-
-                // $account.append($('<h2>');
                 $account.append($h2);
 
                 // This account's transactions
@@ -36,21 +69,7 @@ $(async function () {
                     return (el.account_id == account.id);
                 });
 
-
-                const $ul = $('<ul>');
-                values[1].forEach((fund) => {
-                    const t = transactions.filter((el) => {
-                        return el.fund_id == fund.id;
-                    });
-                    if (!t.length) {
-                        return;
-                    }
-
-                    const q = t.reduce((acc, val) => acc + val.quantity, 0);
-
-                    $ul.append($('<li>').text(`${fund.symbol}: $${(q * quotes[fund.id].price).toFixed(2)} (${q.toFixed(3)} @ $${(quotes[fund.id].price).toFixed(2)})`))
-                });
-                $account.append($ul);
+                $account.append(buildTable(funds, quotes, transactions));
                 $accounts.append($account);
             });
         })
