d6cad65459217a3f0e0b8f9b2ce9ac5fc6f615f2
Stephen <stephen@behnfeldt.pro>
2024-09-26 17:08:38 -0400

    Refactor JS for Positions page, include placeholder for calculating ROI

--- resources/js/positions.js
+++ resources/js/positions.js
@@ -9,13 +9,28 @@ $(async function () {
     const fundsMap  = [];
     const quotesMap = [];
 
-    const buildTable = (funds, quotes, transactions) => {
+    const xirr = (transactions) => {
+        console.log('xirr', transactions);
+        return '0';
+    }
+
+    /**
+     * Build an HTML table summarizing holdings of each fund in the account
+     *
+     * @param account  The account to build the table for
+     * @param funds All the funds
+     * @param quotes All the quotes
+     * @param transactions  All the transactions associated with this account
+     * @returns {*|jQuery|HTMLElement}
+     */
+    const buildTable = (account, funds, quotes, transactions) => {
         let $table = $('<table>');
 
         let $thead = $('<thead>');
         let $tr    = $('<tr>');
         $tr.append($('<th>').text('Fund'));
         $tr.append($('<th>').text('Current Value'));
+        $tr.append($('<th>').text('Annualized ROI'));
         $tr.append($('<th>').text('Quantity'));
         $tr.append($('<th>').text('Current Price'));
         $thead.append($tr);
@@ -25,17 +40,19 @@ $(async function () {
 
         funds.forEach((fund) => {
             const t = transactions.filter((el) => {
-                return el.fund_id == fund.id;
+                return el.account_id == account.id && el.fund_id == fund.id;
             });
             if (!t.length) {
                 return;
             }
+            console.log(fund.symbol);
 
             const q = t.reduce((acc, val) => acc + val.quantity, 0);
 
             const $tr = $('<tr>');
             $tr.append($('<td>').text(`${fund.symbol}`));
             $tr.append($('<td>').text(`$${(q * quotes[fund.id].price).toFixed(2)}`));
+            $tr.append($('<td>').text(Number(xirr(t.filter((el) => el.avg_cost_basis !== 0))).toFixed(2) + '%'));
             $tr.append($('<td>').text(`${q.toFixed(3)}`));
             $tr.append($('<td>').text(`$${(quotes[fund.id].price).toFixed(2)}`));
             $tbody.append($tr);
@@ -44,12 +61,8 @@ $(async function () {
         return $table;
     }
 
-    const buildAccountSection = (account, transactions) => {
-        const $account = $('<section class="account">');
-
-        const $h3 = $('<h3>').text(`${account.company}  ${account.description} (${account.identifier})`);
-        $account.append($h3);
-
+    //
+    const calculateAccountValue = (transactions) => {
         let accountValue = 0;
         fundsMap.forEach((fund) => {
             const t = transactions.filter((el) => {
@@ -61,8 +74,25 @@ $(async function () {
             const q = t.reduce((acc, val) => acc + val.quantity, 0);
             accountValue += q * quotesMap[fund.id].price;
         });
-        $account.append($('<p>').text(`Account Value: $${parseFloat(accountValue.toFixed(2)).toLocaleString('en-US')}`));
-        $account.append(buildTable(fundsMap, quotesMap, transactions));
+        return accountValue;
+
+    }
+
+    /**
+     *
+     * @param account The account being built around
+     * @param transactions The specified account's transactions
+     * @returns {*|jQuery|HTMLElement}
+     */
+    const buildAccountSection = (account, transactions) => {
+        const $account = $('<section class="account">');
+
+        const $h3 = $('<h3>').text(`${account.company}  ${account.description} (${account.identifier})`);
+        $account.append($h3);
+
+        $account.append($('<p>').text(`Account Value: $${parseFloat(calculateAccountValue(transactions).toFixed(2)).toLocaleString('en-US')}`));
+
+        $account.append(buildTable(account, fundsMap, quotesMap, transactions));
 
         return $account;
     }
@@ -86,11 +116,11 @@ $(async function () {
 
             // Build an "account" <section> for each account
             const $accounts = $('.accounts');
+
             values[0].forEach((account) => {
-                const transactions = values[2].filter((el) => {
+                $accounts.append(buildAccountSection(account, values[2].filter((el) => {
                     return (el.account_id === account.id);
-                });
-                $accounts.append(buildAccountSection(account, transactions));
+                })));
             });
         })
         .catch((err) => {
