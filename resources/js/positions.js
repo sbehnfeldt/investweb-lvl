4b85b0e9e21e4308afde4e433ad43bab09a85ab4
Stephen <stephen@behnfeldt.pro>
2024-09-27 06:49:37 -0400

    Calculate annualized return for a single transaction

--- resources/js/positions.js
+++ resources/js/positions.js
@@ -9,9 +9,68 @@ $(async function () {
     const fundsMap  = [];
     const quotesMap = [];
 
-    const xirr = (transactions) => {
-        console.log('xirr', transactions);
-        return '0';
+
+    /**
+     * Calculate and return the ROI based on a single investment
+     *
+     * @param transactions
+     * @param totalQuantity
+     * @param latest_trading_day
+     * @param price
+     * @returns {number}
+     */
+    const getSimpleAnnualizedRoi = ({quantity, avg_cost_basis, acquired}, totalQuantity, {
+        latest_trading_day,
+        price
+    }) => {
+
+        const initialValue     = quantity * avg_cost_basis;
+        const currentValue     = totalQuantity * price;
+        const initialDate      = new Date(acquired);
+        const currentDate      = new Date(latest_trading_day);
+        const differenceInMs   = currentDate - initialDate;
+        const differenceInDays = differenceInMs / (1000 * 60 * 60 * 24);
+
+        return (Math.pow(currentValue / initialValue, 365.25 / differenceInDays) - 1) * 100;
+    }
+
+    /**
+     * Calculate and return the ROI based upon multiple distinct investments
+     *
+     * @param transactions
+     * @param totalQty
+     * @param latest_trading_day
+     * @param price
+     * @returns {number}
+     */
+    const getXirr = (transactions, totalQty, {latest_trading_day, price}) => {
+
+        return 0;
+    }
+
+
+    /**
+     * Get the return on investment for one or more transactions based upon a quote containing the current price
+     *
+     * @param transactions All transactions for a given fund (including transactions with 0 cost basis)
+     * @param quote The market price for a fund on a specified date
+     *
+     * @returns {number}
+     */
+    const getAnnualizedRoi = (transactions, quote) => {
+
+        // Total number of shares in a fund, based on all transactions
+        const quantity = transactions.reduce((acc, cur) => acc + cur.quantity, 0);
+
+        // Total number of actual investments (ie, filter out transactions resulting from dividend re-investment)
+        const t = transactions.filter((el) => el.avg_cost_basis !== 0);
+
+        if (1 === t.length) {
+            // Total number of shares of this fund
+            return getSimpleAnnualizedRoi(t[0], quantity, quote);
+        }
+
+        return getXirr(t, quantity, quote);
     }
 
     /**
@@ -30,9 +89,9 @@ $(async function () {
         let $tr    = $('<tr>');
         $tr.append($('<th>').text('Fund'));
         $tr.append($('<th>').text('Current Value'));
-        $tr.append($('<th>').text('Annualized ROI'));
         $tr.append($('<th>').text('Quantity'));
         $tr.append($('<th>').text('Current Price'));
+        $tr.append($('<th>').text('Annualized ROI'));
         $thead.append($tr);
         $table.append($thead);
 
@@ -45,16 +104,15 @@ $(async function () {
             if (!t.length) {
                 return;
             }
-            console.log(fund.symbol);
 
             const q = t.reduce((acc, val) => acc + val.quantity, 0);
 
             const $tr = $('<tr>');
             $tr.append($('<td>').text(`${fund.symbol}`));
             $tr.append($('<td>').text(`$${(q * quotes[fund.id].price).toFixed(2)}`));
-            $tr.append($('<td>').text(Number(xirr(t.filter((el) => el.avg_cost_basis !== 0))).toFixed(2) + '%'));
             $tr.append($('<td>').text(`${q.toFixed(3)}`));
             $tr.append($('<td>').text(`$${(quotes[fund.id].price).toFixed(2)}`));
+            $tr.append($('<td>').text(Number(getAnnualizedRoi(t, quotes[fund.id])).toFixed(2) + '%'));
             $tbody.append($tr);
         });
         $table.append($tbody);
